c939ee2158fe02367a68fffa82b42fe0070568db
Fernando Alegre <alphalambda@fernando.alemor.org>
2019-06-02 19:22:47 -0500

    Added comments in Operators

--- codeworld-base/src/Lessons/Operators.hs
+++ codeworld-base/src/Lessons/Operators.hs
@@ -12,14 +12,22 @@ module Lessons.Operators(
 
 import Prelude
 
+-- | add the two given numbers
 add :: (Number,Number) -> Number
 add(x,y) = x + y
 
+-- | subtract the first number from the first one
 sub :: (Number,Number) -> Number
 sub(x,y) = x - y
 
+-- | multiply the two given numbers
 mul :: (Number,Number) -> Number
 mul(x,y) = x * y
 
+-- | divide the first number by the second one
 div :: (Number,Number) -> Number
 div(x,y) = x / y
+
+-- | raise the first number to the power of the second one
+pow :: (Number,Number) -> Number
+pow(x,y) = x ^ y
