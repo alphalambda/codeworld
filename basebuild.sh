da8590923432d46ce68f438b8aecdbafb2142f70
Fernando Alegre <alphalambda@fernando.alemor.org>
2021-05-09 18:04:27 -0500

    Use removeHasCallStack in basebuild

--- basebuild.sh
+++ basebuild.sh
@@ -14,9 +14,4 @@ run codeworld-base  cabal haddock --hoogle
 grep -r -s -h 'pattern\s*[A-Za-z_0-9]*\s*::.*' codeworld-base/ \
     >> web/codeworld-base.txt
 
-cd ${cwd}/web/doc
-mv Standard.html Standard.html.pre
-sed -e 's/HasCallStack =&gt; //g' \
-  < Standard.html.pre > Standard.html
-rm Standard.html.pre
-
+sh ./removeHasCallStack.sh
